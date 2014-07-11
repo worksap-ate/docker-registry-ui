64aaba7027850d825d09347fb9fe6d622ca9e5d1
Vinil Narang <vinil@mail.worksap.com>
2014-07-11 11:24:51 +0530

    Edit codes

--- test/app.js
+++ test/app.js
@@ -4,9 +4,9 @@ app.config(function($httpProvider){
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
 
-app.config(function($locationProvider){
+/*app.config(function($locationProvider){
     $locationProvider.html5Mode(true);
-});
+});*/
 
 /*app.config(function($rootScopeProvider){
      $rootScopeProvider.digestTtl(1000); 
